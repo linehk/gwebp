d899bbd92cac8760b4c11be2e0d9207c9ce70628
linehk <sulinehk@gmail.com>
2019-06-21 12:30:12 +0800

    fix flag

--- main.go
+++ main.go
@@ -10,14 +10,15 @@ import (
 )
 
 var (
-	root = *flag.String("r", ".", "root directory")
-	ext  = "." + *flag.String("e", "jpg", "filename extension")
-	args = strings.Split(*flag.String("a", "-q 50", "cwebp arguments"), " ")
-	keep = *flag.Bool("k", true, "keep")
+	root = flag.String("r", ".", "root directory")
+	ext  = flag.String("e", "jpg", "filename extension")
+	args = flag.String("a", "-q 50", "cwebp arguments")
+	keep = flag.Bool("k", true, "keep")
 )
 
 func main() {
-	err := filepath.Walk(root, walkFunc)
+	flag.Parse()
+	err := filepath.Walk(*root, walkFunc)
 	if err != nil {
 		fmt.Printf("filepath.Walk err: %v", err)
 		return
@@ -29,16 +30,16 @@ func walkFunc(path string, info os.FileInfo, err error) error {
 		fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 		return err
 	}
-	if !info.IsDir() && strings.Contains(info.Name(), ext) {
-		newPath := strings.TrimSuffix(path, ext) + ".webp"
-		allArgs := append([]string{path}, args...)
+	if !info.IsDir() && strings.Contains(info.Name(), "."+*ext) {
+		newPath := strings.TrimSuffix(path, "."+*ext) + ".webp"
+		allArgs := append([]string{path}, strings.Split(*args, " ")...)
 		allArgs = append(allArgs, "-o", newPath)
 		cmd := exec.Command("cwebp", allArgs...)
 		err := cmd.Run()
 		if err != nil {
 			return err
 		}
-		if !keep {
+		if !*keep {
 			err := os.Remove(path)
 			if err != nil {
 				return err
